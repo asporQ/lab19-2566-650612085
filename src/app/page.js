dae9768a648ec3cfd1ec0098306bd5069bb1f293
asporQ <120199524+asporQ@users.noreply.github.com>
2023-09-18 15:26:36 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -36,10 +36,12 @@ export default function Home() {
   };
 
   const loadMyCourses = async () => {
+    setLoadingMyCourses(true);
     const resp = await axios.get("/api/enrollment", {
       headers: { Authorization: `Bearer ${token}` },
     });
     setMyCourses(resp.data.courses);
+    setLoadingMyCourses(false);
   };
 
   useEffect(() => {
@@ -53,16 +55,19 @@ export default function Home() {
   }, [token]);
 
   const login = async () => {
+    setLoadingLogin(true);
     try {
       const resp = await axios.post("/api/user/login", { username, password });
       setToken(resp.data.token);
       setAuthenUsername(resp.data.username);
       setUsername("");
       setPassword("");
+      setLoadingLogin(false);
     } catch (error) {
       if (error.response.data) {
         alert(error.response.data.message);
       }
+      setLoadingLogin(false);
     }
   };
 
@@ -105,7 +110,12 @@ export default function Home() {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
-              <Button onClick={login}>Login</Button>
+              {!loadingLogin && <Button onClick={login}>Login</Button>}
+              {loadingLogin && (
+                <Button disabled onClick={login}>
+                  Login...
+                </Button>
+              )}
             </Group>
           )}
           {authenUsername && (
@@ -133,9 +143,13 @@ export default function Home() {
             ))}
 
           {/* Do something with below loader!! */}
-          <Loader variant="dots" />
+          {loadingMyCourses && <Loader variant="dots" />}
         </Paper>
-        <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer
+          year="2023"
+          fullName="Tikomporn Luxsanaprokin"
+          studentId="650612085"
+        />
       </Stack>
     </Container>
   );
